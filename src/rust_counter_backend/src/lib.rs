cc7c659f225142dce4fdff94b690aa231ff5d6c5
EmmanuelHaggai <emmanuelhaggai85@gmail.com>
2024-03-13 20:50:00 +0300

    Initial commit

--- src/rust_counter_backend/src/lib.rs
+++ src/rust_counter_backend/src/lib.rs
@@ -1,4 +1,24 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use std::cell::RefCell;
+use candid::types::number::Nat;
+
+thread_local! {
+    static COUNTER: RefCell<Nat> = RefCell::new(Nat::from(0));
+}
+
+// Get the value of the counter.
+#[ic_cdk_macros::query]
+fn get() -> Nat {
+    COUNTER.with(|counter| (*counter.borrow()).clone())
+}
+
+// Set the value of the counter.
+#[ic_cdk_macros::update]
+fn set(n: Nat) {
+    COUNTER.with(|count| *count.borrow_mut() = n);
+}
+
+// Increment the value of the counter.
+#[ic_cdk_macros::update]
+fn increment() {
+    COUNTER.with(|counter| *counter.borrow_mut() += 1);
 }
